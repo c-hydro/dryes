a6ca0b3a3bb235048f3e8eac8916b29090b53aaa
michelisabellon <michel.isabellon@cimafoundation.org>
2023-10-12 17:34:43 +0200

    commit_20231012_17:34

--- tools/tool_index_classifier/DRYES_tool_index_classifier.sh
+++ tools/tool_index_classifier/DRYES_tool_index_classifier.sh
@@ -2,9 +2,9 @@
 
 #-----------------------------------------------------------------------------------------
 # Script information
-script_name='DRYES Algorithm to aggregate rasters according to regions (e.g., NUTS)'
-script_version="1.0.1"
-script_date='2023/09/28'
+script_name='DRYES Algorithm to classify tiff objects by defined thresholds and classes'
+script_version="1.0.0"
+script_date='2023/10/12'
 
 virtualenv_folder=''
 virtualenv_name=''
@@ -20,8 +20,8 @@ script_file='dryes_index_classifier.py'
 settings_file='dryes_tool_index_classifier.json'
 
 # Get information (-u to get gmt time)
-#time_now=$(date -u +"%Y-%m-%d %H:00")
-time_now='2023-09-01 00:00' # DEBUG 
+time_now=$(date -u +"%Y-%m-%d %H:00")
+#time_now='2023-08-01 00:00' # DEBUG
 #-----------------------------------------------------------------------------------------
 
 #-----------------------------------------------------------------------------------------

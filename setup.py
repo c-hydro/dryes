a9a9af849a2d85d2e1cfa6e845f9e982ee11e108
ltrotter <lucatrotter@gmail.com>
2024-10-22 14:00:18 +0200

    chore: update version in setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dryes',
-    version='3.1.0-alpha',
+    version='3.1.0',
     packages=find_packages(),
     description='A package for operational calculation of environmental indices for drought monitoring',
     author='Luca Trotter',

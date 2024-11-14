7688d875854ef4725ca532fda0b6aa36a6f0063c
ltrotter <lucatrotter@gmail.com>
2024-11-14 17:43:57 +0100

    fix(indicators): changes to ensure dryes indicator works

--- dryes/indices/dryes_indicator.py
+++ dryes/indices/dryes_indicator.py
@@ -1,9 +1,9 @@
 from datetime import datetime
-from typing import Iterable
+from typing import Sequence
 from copy import deepcopy
 
-from dryes.tools.timestepping.fixed_num_timestep import FixedNTimeStep
-from dryes.tools.timestepping.timerange import TimeRange
+from ..tools.timestepping.fixed_num_timestep import FixedNTimeStep
+from ..tools.timestepping.timerange import TimeRange
 
 from .dryes_index import DRYESIndex
 from ..index_combination import COMBINATION_ALGORITHMS
@@ -12,27 +12,66 @@ class DRYESCombinedIndicator(DRYESIndex):
     index_name = 'indicator'
     default_options = {
         'algorithm': 'cdi_jrc',
+        'args': {}
     }
 
     @property
     def parameters(self):
-        input_data, previous_data, output_data = self.algorithm()
+        input_data, previous_data, static_data, output_data = self.algorithm()
         input_keys = list(k for k in input_data)
         previous_keys = list(k for k in previous_data)
+        static_keys = list(k for k in static_data)
         output_keys = list(k for k in output_data)
-        return input_keys + previous_keys + output_keys[1:]
+        return input_keys + previous_keys + static_keys + output_keys[1:]
 
     def _check_index_options(self, options: dict) -> None:
         super()._check_index_options(options)
         self.algorithm = COMBINATION_ALGORITHMS.get(self.options.pop('algorithm'))
+        self.args = self.options.pop('args', {})
+
+    def compute(self, current:   Sequence[datetime]|TimeRange|None = None,
+                      timesteps_per_year: int|None = None) -> None:
+
+        # set current and reference periods
+        current   = self.as_time_range(current, 'current')
+        if current is None:
+            if not self.options['make_parameters']:
+                raise ValueError('No current period specified.')
+            else:
+                #TODO add a warning that we are only calculating parameters
+                pass
+        
+        # set timesteps per year
+        if timesteps_per_year is None:
+            if hasattr(self, 'timesteps_per_year'):
+                timesteps_per_year = self.timesteps_per_year
+            else:
+                raise ValueError('No timesteps per year specified.')
+
+        if len(self._tiles) == 0:
+            self._compute_tile(current, timesteps_per_year)
+        else:
+            for tile in self._tiles:
+                new_self = self.update_io(tile = tile)
+                new_self._compute_tile(current, timesteps_per_year)
+
+    def _compute_tile(self, current: TimeRange, timesteps_per_year: int) -> None:
+        # calculate the index
+        if current is not None:
+            self.make_index(current, timesteps_per_year)
 
     def _check_io_options(self, io_options: dict, update_existing = False) -> None:
+
+        self.io_options = io_options
+        self._tiles = []
 
         # get a template from any of the input datasets
         io_names = self.parameters
         for name in io_names:
             ds       = io_options.get(name)
-            template = ds.get_template(**self.cases['opt'][0]['options'])
+            if ds.has_tiles: self._tiles = ds.tile_names #TODO: check that all datasets have the same tiles
+            template = ds.get_template_dict(**self.cases['opt'][0]['options'])
+            #template = ds.get_template(**self.cases['opt'][0]['options'])
             if template is not None:
                 self.output_template = template
                 break
@@ -46,7 +85,7 @@ class DRYESCombinedIndicator(DRYESIndex):
         pass
 
     def make_parameters(self,
-                        history: TimeRange|Iterable[datetime],
+                        history: TimeRange|Sequence[datetime],
                         timesteps_per_year: int) -> None:
         
         self.log.warning('No parameters to calculate for this index.')
@@ -56,14 +95,28 @@ class DRYESCombinedIndicator(DRYESIndex):
                    time: FixedNTimeStep,
                    case: dict) -> tuple:
         
-        input_data, previous_data, output_data = self.algorithm()
-        input_keys = list(k for k in input_data)
+        input_data, previous_data, static_data, output_data = self.algorithm()
+        input_keys    = list(k for k in input_data)
         previous_keys = list(k for k in previous_data)
+        static_keys   = list(k for k in static_data)
 
         this_input_data    = {k:self._parameters[k].get_data(time, **case['tags']) for k in input_keys}
-        this_previous_data = {k:self._parameters[k].get_data(time-1, **case['tags']) for k in previous_keys}
 
-        this_output_data = self.algorithm(this_input_data, this_previous_data)
+        this_previous_data = {}
+        for k in previous_keys:
+            try:
+                this_previous_data[k] = self._parameters[k].get_data(time-1, **case['tags'])
+            except ValueError:
+                this_previous_data[k] = self._parameters[k].build_templatearray(self.output_template)
+
+        this_static_data = {}
+        for k in static_keys:
+            try:
+                this_static_data[k] = self._parameters[k].get_data(**case['tags'])
+            except ValueError:
+                this_static_data[k] = None
+
+        this_output_data = self.algorithm(this_input_data, this_previous_data, this_static_data, **self.args)
         self.save_output_data(this_output_data, time, case['options'], **case['options'])
 
         index_name = output_data[0]
@@ -82,7 +135,7 @@ class DRYESCombinedIndicator(DRYESIndex):
                          time: FixedNTimeStep,
                          metadata: dict, **tags) -> None:
         
-        _, _, ouptut_data = self.algorithm()
+        _, _, _, ouptut_data = self.algorithm()
         output_keys = list(k for k in ouptut_data)
 
         for key in output_keys:
@@ -92,7 +145,7 @@ class DRYESCombinedIndicator(DRYESIndex):
             this_ds.write_data(data[key], time = time, metadata = metadata, **tags)
 
     def make_index(self,
-                   current:   TimeRange|Iterable[datetime],
+                   current:   TimeRange|Sequence[datetime],
                    timesteps_per_year: int) -> None:
 
         if isinstance(current, tuple) or isinstance(current, list):

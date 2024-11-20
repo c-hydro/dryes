7a15b9ba49212380bab3a2fc5c414456cdffa695
michelisabellon <michel.isabellon@cimafoundation.org>
2024-11-20 13:52:35 +0100

    feat(index_combination): add risk_algorithms

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dryes',
-    version='3.1.2',
+    version='3.1.4',
     packages=find_packages(),
     description='A package for operational calculation of environmental indices for drought monitoring',
     author='Luca Trotter',
